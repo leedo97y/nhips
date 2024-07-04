7e63ed257de4278e8794a5075fdda4dc3f7f5b0f
leedo97y <dlehdus1433@gmail.com>
2024-07-04 20:38:50 +0900

    feat: 크롤링 데이터 추가
    - cheerios를 사용한 네이버 뉴스 데이터 크롤링
    - 크롤링 데이터 type 추가
    - cors 에러 해결을 위한 rewrites 코드 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // cors 에러 해결
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `${process.env.NEXT_PUBLIC_NEWS_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 import Layout from "@/pages/components/Layout";
+import { NewsContentsType } from "@/types/DataType";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+const Home = () => {
+  const [re, _setRe] = useState(false);
+  const [data, setData] = useState<NewsContentsType[]>([
+    { id: 0, imgLink: "", headline: "", link: "", date: "", pressInfo: "", content: "" },
+  ]);
+
+  useEffect(() => {
+    axios.get(`/api/crawler`).then((res) => {
+      console.log(res);
+      setData(res.data);
+    });
+  }, [re]);
+
   return (
     <Layout>
       <div></div>
     </Layout>
   );
-}
+};
+
+export default Home;
