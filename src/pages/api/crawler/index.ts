4973e7c368ba9c0a51b09e3e4729f8ec51a59308
leedo97y <dlehdus1433@gmail.com>
2024-07-04 20:38:50 +0900

    feat: 크롤링 데이터 추가
    - cheerios를 사용한 네이버 뉴스 데이터 크롤링
    - 크롤링 데이터 type 추가
    - cors 에러 해결을 위한 rewrites 코드 추가

---
+++ src/pages/api/crawler/index.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { load } from "cheerio";
+import { NewsContentsType } from "@/types/DataType";
+
+const getData = async (url: string) => {
+  try {
+    const html = await axios.get(url);
+    const $ = load(html.data);
+
+    let content: NewsContentsType[] = [];
+    const ARTICLE_SELECTOR = $("div.section_latest_article > div > ul.sa_list > li > div > div");
+
+    ARTICLE_SELECTOR.map((idx, el) => {
+      content[idx] = {
+        id: idx,
+        headline: $(el).find(".sa_text > a > strong").text(),
+        link: $(el).find("div.sa_thumb > .sa_thumb_inner > a.sa_thumb_link").attr("href"),
+        pressInfo: $(el).find(".sa_text_info_left > .sa_text_press").text(),
+        date: $(el).find(".sa_text_info_left > .sa_text_datetime > b").text(),
+        content: $(el).find(".sa_text_lede").text(),
+        imgLink: $(el).find("div.sa_thumb > .sa_thumb_inner > a.sa_thumb_link > img").attr("src"),
+      };
+    });
+
+    return content;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const newsData = getData(process.env.NEXT_PUBLIC_NEWS_URL || "");
+
+const crawlerHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json(await newsData);
+};
+
+export default crawlerHandler;
