8d12ed2eb4cc9c5cf1fc0a466dcfa9c7f8da0fa8
leedo97y <dlehdus1433@gmail.com>
2024-07-08 18:31:10 +0900

    feat: 메인 페이지 추가
    - 메인 페이지 추가
    - 폰트 파일 추가 및 적용
    - 페이지별 footer 분리
    - 페이지별 API 분리 및 해당 사항 .env, next config, crawler 적용
    - tailwind config color 추가, font-family 추가
    - 페이지별 버튼 이미지 추가

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -1,11 +1,25 @@
 const Footer = () => {
   return (
-    <footer className="flex flex-col gap-2 justify-center items-center bg-GRAY_EXTRADARK h-28 text-xs text-white dark:bg-GRAY_EXTRAHEAVY dark:text-GRAY_HEAVY">
+    <footer className="sticky bottom-0 w-[100vw] h-20 flex flex-col gap-2 justify-center items-center dark:bg-GRAY_EXTRADARK text-xs text-white dark:bg-GRAY_EXTRAHEAVY dark:text-GRAY_HEAVY">
       <div className="flex flex-col items-center">
-        <span className="whitespace-pre-wrap text-center">{`Copyright 2023. doyeonlee All rights reserved.`}</span>
+        <span className="whitespace-pre-wrap text-center">
+          Copyright 2023. doyeonlee All rights reserved.
+        </span>
       </div>
     </footer>
   );
 };
 
-export default Footer;
+const FixedFooter = () => {
+  return (
+    <footer className="fixed bottom-0 w-[100vw] h-20 flex flex-col gap-2 justify-center items-center bg-FOOTER_BG_LIGHT dark:bg-GRAY_EXTRADARK text-xs dark:text-white dark:bg-GRAY_EXTRAHEAVY dark:text-GRAY_HEAVY">
+      <div className="flex flex-col items-center">
+        <span className="whitespace-pre-wrap text-center">
+          Copyright 2023. doyeonlee All rights reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export { FixedFooter, Footer };
