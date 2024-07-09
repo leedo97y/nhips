647d0b4fc283abedf75afbb245844178c7dca26e
leedo97y <dlehdus1433@gmail.com>
2024-07-10 01:25:53 +0900

    feat: IT/Science news 페이지 추가
    - IT/Science news rack 추가
    - main home 배너 글 수정
    - xs screen size 추가
    - politics 아이콘 수정
    - footer, header 모바일 사이즈 적용
    - mobile 화면 Header 추가
    - main 페이지 mobile 사이즈 적용

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
 
 const FixedFooter = () => {
   return (
-    <footer className="fixed bottom-0 w-[100vw] h-20 flex flex-col gap-2 justify-center items-center bg-FOOTER_BG_LIGHT dark:bg-GRAY_EXTRADARK text-xs dark:text-white dark:bg-GRAY_EXTRAHEAVY dark:text-GRAY_HEAVY">
+    <footer className="fixed bottom-0 w-[100vw] h-20 flex flex-col gap-2 justify-center items-center bg-FOOTER_BG_LIGHT dark:bg-GRAY_EXTRADARK md:text-xs text-[10px] dark:text-white dark:bg-GRAY_EXTRAHEAVY dark:text-GRAY_HEAVY">
       <div className="flex flex-col items-center">
         <span className="whitespace-pre-wrap text-center">
           Copyright 2024. doyeonlee All rights reserved.
