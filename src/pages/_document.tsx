9b2db295756453a7e2ff3e42725eb674d3eb384c
leedo97y <dlehdus1433@gmail.com>
2024-07-15 23:16:30 +0900

    fix: SEO 추가 및 meta 추가
    - header에 httpEquiv 추가
    - next-seo 추가
    - SEO를 위한 코드 _app에 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <body className="font-normal font-GABIA break-keep selection:bg-PRIMARY_LIGHT selection:dark:text-BLACK text-BLACK dark:bg-BLACK dark:text-white">
         <Main />
         <NextScript />
