ff945da32f7ea53db99f187ff77b3271d534cbb2
leedo97y <dlehdus1433@gmail.com>
2024-07-17 01:03:57 +0900

    fix: loading 추가
    - 데이터 로딩 시간 지연에 따른 loading 텍스트 추가
    - 커스텀 API에서 불러온 id NaN 에러 발생으로 인해 list id로 수정

--- src/pages/components/NewsItems/NewsList.tsx
+++ src/pages/components/NewsItems/NewsList.tsx
@@ -1,26 +1,36 @@
 import Image from "next/image";
 import { PressContentType } from "@/types/DataType";
 
-const NewsList = ({ id, headline, imgSrc, link, viewCount }: PressContentType) => {
+const NewsList = ({ list_id, headline, imgSrc, link, viewCount }: PressContentType & any) => {
   return (
-    <div className="md:w-96 w-full max-h-full p-3 flex gap-3 dark:bg-CARD_BG_DARK bg-CARD_BG_LIGHT rounded-sm">
-      <span className="md:w-[30px] w-[20px] font-HAKGYO md:text-lg text-base">{id + 1}</span>
-      {imgSrc && (
-        <a href={link} target="_blank" rel="noreferrer">
-          <div className="w-[100px] md:h-[75px] h-[90px] flex justify-center">
-            <Image src={imgSrc} alt="News Image" width={100} height={100} />
+    <div className="md:w-96 w-full max-h-full p-3 dark:bg-CARD_BG_DARK bg-CARD_BG_LIGHT rounded-sm">
+      {headline ? (
+        <div className="flex gap-3">
+          <span className="md:w-[30px] w-[20px] font-HAKGYO md:text-lg text-base">
+            {list_id + 1}
+          </span>
+          {imgSrc && (
+            <a href={link} target="_blank" rel="noreferrer">
+              <div className="w-[100px] md:h-[75px] h-[90px] flex justify-center">
+                <Image src={imgSrc} alt="News Image" width={100} height={100} />
+              </div>
+            </a>
+          )}
+          <div className="flex flex-col gap-3 justify-between">
+            <a href={link} target="_blank" rel="noreferrer">
+              <h4 className="text-xs">{headline}</h4>
+            </a>
+            <p className="text-xs flex gap-2">
+              <span className="">view</span>
+              {viewCount}
+            </p>
           </div>
-        </a>
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <p className="font-HAKGYO md:text-lg">Loading...</p>
+        </div>
       )}
-      <div className="flex flex-col gap-3 justify-between">
-        <a href={link} target="_blank" rel="noreferrer">
-          <h4 className="text-xs">{headline}</h4>
-        </a>
-        <p className="text-xs flex gap-2">
-          <span className="">view</span>
-          {viewCount}
-        </p>
-      </div>
     </div>
   );
 };
