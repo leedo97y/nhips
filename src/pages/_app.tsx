b3d01121f979b2703552874792413eadff92b2c2
leedo97y <dlehdus1433@gmail.com>
2024-11-06 21:07:13 +0900

    feat: #18 SEO 설정을 위한 sitemap, robot, .env.local 추가, date-fns 패키지 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,21 +6,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
 const DEFAULT_SEO = {
-  title: "Nhips",
+  title: "Nhips | 간편한 뉴스",
   description: "간식처럼 간편하게 뉴스를 즐길 수 있게 만드는 웹 서비스 입니다.",
   canonical: "https://nhips.vercel.app/",
   openGraph: {
     type: "website",
     locale: "ko_KR",
     url: "https://nhips.vercel.app/",
-    title: "Nhips",
-    site_name: "Nhips",
+    title: "Nhips | 간편한 뉴스",
+    site_name: "Nhips | 간편한 뉴스",
     images: [
       {
         url: "/share.png",
         width: 285,
         height: 167,
-        alt: "Nhips",
+        alt: "Nhips | 간편한 뉴스",
       },
     ],
   },
@@ -33,11 +33,11 @@ const DEFAULT_SEO = {
   additionalMetaTags: [
     {
       name: "application-name",
-      content: "Nhips",
+      content: "Nhips | 간편한 뉴스",
     },
     {
       name: "msapplication-tooltip",
-      content: "Nhips",
+      content: "Nhips | 간편한 뉴스",
     },
     {
       name: "viewport",
---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,52 @@
+import { GetServerSideProps } from "next";
+import { format } from "date-fns";
+
+const Sitemap = () => {
+  return null;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://doyeonlee.vercel.app/";
+
+  // 정적 페이지 URL 목록
+  const staticPages = [
+    `${baseUrl}/`,
+    `${baseUrl}/social`,
+    `${baseUrl}/world`,
+    `${baseUrl}/economy`,
+    `${baseUrl}/politics`,
+    `${baseUrl}/it-science`,
+  ];
+
+  // 동적 라우트의 pressname 값을 설정
+  const pressnames = ["YH", "MBC", "SBS", "MBN", "KBS", "YTN", "CA", "HAN"];
+  const dynamicPages = pressnames.map((pressname) => `${baseUrl}/ranking/${pressname}`);
+
+  // 모든 페이지 URL 통합
+  const allPages = [...staticPages, ...dynamicPages];
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${allPages
+      .map((url) => {
+        return `
+          <url>
+            <loc>${url}</loc>
+            <lastmod>${format(new Date(), "yyyy-MM-dd")}</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>0.7</priority>
+          </url>
+        `;
+      })
+      .join("")}
+  </urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
